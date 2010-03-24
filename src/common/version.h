a4dcc5af8b0019d910a0c828fc3235b916252d40
XpycT <XpycT.TOP@gmail.com>
2010-03-24 16:51:09 +0200

    add license

--- src/common/version.h
+++ src/common/version.h
@@ -1,11 +1,31 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2010 by Tarabaka Alexander <XpycT.TOP@gmail.com>   *
+ *                                                                         *
+ *  This file is part of Template Monster Browser.			   *
+ *									   *
+ *  Template Monster Browser is free software: you can redistribute it 	   *
+ *  and/or modify it under the terms of the GNU General Public License as  *
+ *  published by the Free Software Foundation, either version 3 of the     *
+ *  License, or (at your option) any later version.			   *
+ *									   *
+ *  Template Monster Browser is distributed in the hope that it will 	   *
+ *  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty *
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	   *
+ *  GNU General Public License for more details.			   *
+ *									   *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with Template Monster Browser.                                   *
+ *  If not, see <http://www.gnu.org/licenses/>.                            *
+ ***************************************************************************/
+
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define TMBROWSER_VERSION      "1.2.1.8"
-#define TMBROWSER_VERSION_RC   1,2,1,8
+#define TMBROWSER_VERSION      "1.2.2.6"
+#define TMBROWSER_VERSION_RC   1,2,2,6
 #define TMBROWSER_NAME         "Template Monster Browser"
 #define TMBROWSER_ORGANIZATION "XpycT & AlEXANDER"
-#define TMBROWSER_DOMAIN       "http://code.google.com/p/template-monster-browser/"
+#define TMBROWSER_DOMAIN       "http://github.com/XpycT/Template-Monster-Browser"
 #define TMBROWSER_COPYRIGHT    "Copyright © 2009-2010 XpycT & AlEXANDER"
 
 #endif /*VERSION_H_*/
